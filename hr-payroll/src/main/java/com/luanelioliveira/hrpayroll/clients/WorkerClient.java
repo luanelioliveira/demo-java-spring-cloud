55b229ec6e539e287a292ec5868a11cf74c1cf6e
Luan Eli Oliveira <luan.oliveira@olxbr.com>
2021-05-19 08:58:11 -0300

    adding eureka client

--- hr-payroll/src/main/java/com/luanelioliveira/hrpayroll/clients/WorkerClient.java
+++ hr-payroll/src/main/java/com/luanelioliveira/hrpayroll/clients/WorkerClient.java
@@ -1,14 +1,12 @@
 package com.luanelioliveira.hrpayroll.clients;
 
 import com.luanelioliveira.hrpayroll.models.Worker;
-import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 @Component
-@RibbonClient(name = "hr-worker")
 @FeignClient(name = "hr-worker", path = "/workers")
 public interface WorkerClient {
 
