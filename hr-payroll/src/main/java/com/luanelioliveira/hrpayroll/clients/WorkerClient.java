d0ba9ccc7ddadb2d6e3ea55041c908ff63924112
Luan Eli Oliveira <luan.oliveira@olxbr.com>
2021-05-19 08:29:28 -0300

    adding ribbon client

--- hr-payroll/src/main/java/com/luanelioliveira/hrpayroll/clients/WorkerClient.java
+++ hr-payroll/src/main/java/com/luanelioliveira/hrpayroll/clients/WorkerClient.java
@@ -1,13 +1,15 @@
 package com.luanelioliveira.hrpayroll.clients;
 
 import com.luanelioliveira.hrpayroll.models.Worker;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 @Component
-@FeignClient(name = "worker", url = "http://localhost:8001", path = "/workers")
+@RibbonClient(name = "hr-worker")
+@FeignClient(name = "hr-worker", path = "/workers")
 public interface WorkerClient {
 
   @GetMapping("/{id}")
--- hr-payroll/src/main/java/com/luanelioliveira/hrpayroll/configs/FeignClient.java
+++
@@ -1,8 +0,0 @@
-package com.luanelioliveira.hrpayroll.configs;
-
-import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@EnableFeignClients(basePackages = "com.luanelioliveira.hrpayroll.clients")
-public class FeignClient {}
