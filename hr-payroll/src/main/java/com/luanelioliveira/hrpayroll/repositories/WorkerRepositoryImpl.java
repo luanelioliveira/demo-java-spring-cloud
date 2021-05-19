5397b3a69bdab84906108cf120971f42c5529a92
Luan Eli Oliveira <luan.oliveira@olxbr.com>
2021-05-19 09:28:36 -0300

    adding hystrix and circuit breaker

--- hr-payroll/src/main/java/com/luanelioliveira/hrpayroll/repositories/WorkerRepositoryImpl.java
+++ hr-payroll/src/main/java/com/luanelioliveira/hrpayroll/repositories/WorkerRepositoryImpl.java
@@ -2,26 +2,30 @@ package com.luanelioliveira.hrpayroll.repositories;
 
 import com.luanelioliveira.hrpayroll.clients.WorkerClient;
 import com.luanelioliveira.hrpayroll.models.Worker;
-import feign.FeignException;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import java.util.Optional;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Repository;
 
+@Slf4j
 @RequiredArgsConstructor
 @Repository
 public class WorkerRepositoryImpl implements WorkerRepository {
 
   private final WorkerClient client;
 
+  @HystrixCommand(fallbackMethod = "getWorkerByIdFallback")
   @Override
   public Optional<Worker> getWorkerById(Long id) {
+    var worker = client.getWorkerById(id);
+    return Optional.ofNullable(worker);
+  }
 
-    try {
-      var worker = client.getWorkerById(id);
-      return Optional.ofNullable(worker);
-    } catch (FeignException exception) {
-      if (exception.status() != 404) throw exception;
-      return Optional.empty();
-    }
+  public Optional<Worker> getWorkerByIdFallback(Long id) {
+    log.info("Calling getWorkerById fallback");
+
+    var worker = new Worker(999L, "Brann", 400.0);
+    return Optional.ofNullable(worker);
   }
 }
--- hr-payroll/src/main/java/com/luanelioliveira/hrpayroll/resources/PaymentResource.java
+++ hr-payroll/src/main/java/com/luanelioliveira/hrpayroll/resources/PaymentResource.java
@@ -3,12 +3,14 @@ package com.luanelioliveira.hrpayroll.resources;
 import com.luanelioliveira.hrpayroll.models.Payment;
 import com.luanelioliveira.hrpayroll.services.PaymentService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequestMapping("/payments")
 @RequiredArgsConstructor
