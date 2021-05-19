90dd0e9e64761ad6fed5fb57b3714263fa855fa0
Luan Eli Oliveira <luan.oliveira@olxbr.com>
2021-05-19 09:43:37 -0300

    configuring gateway server

--- hr-gateway-server/src/main/java/com/luanelioliveira/hrgatewayserver/HrGatewayServerApplication.java
+++ hr-gateway-server/src/main/java/com/luanelioliveira/hrgatewayserver/HrGatewayServerApplication.java
@@ -2,12 +2,15 @@ package com.luanelioliveira.hrgatewayserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
+@EnableZuulProxy
+@EnableEurekaClient
 @SpringBootApplication
 public class HrGatewayServerApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(HrGatewayServerApplication.class, args);
   }
-
 }
