aa67e21e339190fc3d95f64ef7cc9d970f25f4c6
Luan Eli Oliveira <luan.oliveira@olxbr.com>
2021-05-21 08:30:53 -0300

    adding gateway authorization

---
+++ hr-gateway-server/src/main/java/com/luanelioliveira/hrgatewayserver/configs/JwtConfig.java
@@ -0,0 +1,22 @@
+package com.luanelioliveira.hrgatewayserver.configs;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+
+@Configuration
+public class JwtConfig {
+
+  @Bean
+  public JwtAccessTokenConverter accessTokenConverter() {
+    var converter = new JwtAccessTokenConverter();
+    converter.setSigningKey("MY-SECRET-KEY");
+    return converter;
+  }
+
+  @Bean
+  public JwtTokenStore tokenStore() {
+    return new JwtTokenStore(accessTokenConverter());
+  }
+}
